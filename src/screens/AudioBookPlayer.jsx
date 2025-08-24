87a936d7a98397c8f8a6af13203c13db27c67900
Srijon Chowdhury <csrijon92@gmail.com>
2025-08-25 02:24:18 +0530

    Tody Work done

--- src/screens/AudioBookPlayer.jsx
+++ src/screens/AudioBookPlayer.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { SafeAreaView, View, StyleSheet, TouchableOpacity, Image, Text,StatusBar } from "react-native";
+import { SafeAreaView, View, StyleSheet, TouchableOpacity, Image, Text, StatusBar } from "react-native";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Slider from "@react-native-community/slider";
 
 
-const AudioBookPlayer = ({navigation}) => {
+const AudioBookPlayer = ({ navigation }) => {
     const insets = useSafeAreaInsets()
     return (
         <SafeAreaView style={[styles.container, { paddingTop: insets.top }]} >
-           <StatusBar translucent backgroundColor="#6C3EB4" barStyle="dark-content" />
+            <StatusBar translucent backgroundColor="#000000" barStyle="Light-content" />
             {/* {Top Icon} */}
             <View style={styles.topcontainer} >
                 {/* {back-arrow-button} */}
-                <TouchableOpacity onPress={()=> navigation.goBack()} >
-                    <MaterialIcons name="arrow-back-ios" size={22} color="#000000" />
+                <TouchableOpacity onPress={() => navigation.goBack()} >
+                    <MaterialIcons name="arrow-back-ios" size={22} color="#66B2FF" />
                 </TouchableOpacity>
                 {/* {Notification icon} */}
                 <TouchableOpacity>
-                    <MaterialIcons name="notifications" size={22} color="#000000" />
+                    <MaterialIcons name="notifications" size={22} color="#66B2FF" />
                 </TouchableOpacity>
             </View>
             {/* image section */}
@@ -29,10 +29,10 @@ const AudioBookPlayer = ({navigation}) => {
             <View style={styles.Namesection}  >
                 <View style={styles.Textsection} >
                     <Text style={styles.Boldtext} >ATOMIC HABITS</Text>
-                    <Text>James Clear</Text>
+                    <Text style={styles.normal} >James Clear</Text>
                 </View>
                 <TouchableOpacity>
-                    <MaterialIcons name="favorite" size={22} color="#fff" />
+                    <MaterialIcons name="favorite" size={22} color="#66B2FF" />
                 </TouchableOpacity>
             </View>
             {/* seekbar */}
@@ -40,39 +40,39 @@ const AudioBookPlayer = ({navigation}) => {
                 style={styles.seekbar}
                 minimumValue={0}
                 maximumValue={500}
-                minimumTrackTintColor="#23085A"
-                maximumTrackTintColor="#9D83D2"
-                thumbTintColor="#23085A"
+                minimumTrackTintColor="#66B2FF"
+                maximumTrackTintColor="white"
+                thumbTintColor="#66B2FF"
             />
             {/* Timeline */}
             <View style={styles.Timeline} >
-                <Text  >4:25</Text>
-                <Text>1:25:00</Text>
+                <Text style={styles.TimelineText}>4:25</Text>
+                <Text style={styles.TimelineText} >1:25:00</Text>
             </View>
             {/* playsection  */}
             <View style={styles.PlayMode}  >
                 <TouchableOpacity>
-                    <MaterialIcons name="replay-10" size={30} color="#23085A" />
+                    <MaterialIcons name="replay-10" size={30} color="#66B2FF" />
                 </TouchableOpacity>
                 <TouchableOpacity>
-                    <MaterialIcons name="skip-previous" size={30} color="#23085A" />
+                    <MaterialIcons name="skip-previous" size={30} color="#66B2FF" />
                 </TouchableOpacity>
                 <View style={styles.playButtonWrapper} >
                     <TouchableOpacity>
-                        <MaterialIcons name="play-arrow" size={24} color="#23085A" />
+                        <MaterialIcons name="play-arrow" size={24} color="#66B2FF" />
                     </TouchableOpacity>
                 </View>
 
                 <TouchableOpacity>
-                    <MaterialIcons name="skip-next" size={30} color="#23085A" />
+                    <MaterialIcons name="skip-next" size={30} color="#66B2FF" />
                 </TouchableOpacity>
 
                 <TouchableOpacity>
-                    <MaterialIcons name="forward-10" size={30} color="#23085A" />
+                    <MaterialIcons name="forward-10" size={30} color="#66B2FF" />
                 </TouchableOpacity>
             </View>
             {/* add switch */}
-            <View style ={[styles.bottomswitch,{paddingBottom:insets.bottom}]} >
+            <View style={[styles.bottomswitch, { paddingBottom: insets.bottom }]} >
                 <Text>Up next</Text>
                 <Text>Lyrics</Text>
                 <Text>Related</Text>
@@ -87,9 +87,9 @@ export default AudioBookPlayer;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#EADDFF',
+        backgroundColor: '#000000',
         paddingHorizontal: 20,
-        color: '#000000'
+        color: '#66B2FF'
     },
     topcontainer: {
         flexDirection: 'row',
@@ -122,7 +122,11 @@ const styles = StyleSheet.create({
     },
     Boldtext: {
         fontSize: 14,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+    normal: {
+        color: '#ffffff'
     },
     seekbar: {
         width: '100%',
@@ -130,10 +134,10 @@ const styles = StyleSheet.create({
     },
     Timeline: {
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
     },
     TimelineText: {
-        color: '#23085A'
+        color: '#66B2FF'
     },
     PlayMode: {
         flexDirection: 'row',
@@ -143,13 +147,13 @@ const styles = StyleSheet.create({
         gap: 20
     },
     playButtonWrapper: {
-        backgroundColor: '#b19cd9',
+        backgroundColor: '#ffffff',
         borderRadius: 50,
         padding: 15,
     },
-    bottomswitch:{
+    bottomswitch: {
         marginTop: 90,
-        flexDirection:'row',
+        flexDirection: 'row',
         justifyContent: 'space-around'
     }
 })
